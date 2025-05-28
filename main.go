fc10cc627f3e0d3bd9ea8b952aa555eea24eb55b
0x7b <the0x7b@proton.me>
2025-05-29 00:07:36 +0300

    Update 1.2

--- main.go
+++ main.go
@@ -16,22 +16,25 @@ import (
 	"github.com/rivo/tview"
 )
 
-var packetsView *tview.TextView
+var packetsView *tview.List
 var filterForm *tview.Form
 var ifcname string
-var payloads bool
+var payloads bool = true
 var paused bool
-var maxlines int = 500
+var maxlines int = 1000
 var writer *pcapgo.Writer
 var write bool
 var filename string = "packets.pcap"
 var statistic *tview.TextView
-var tcp, udp, icmp int = 0, 0, 0
+var tcpCounter, udpCounter, icmpCounter int = 0, 0, 0
+var info []string
+var packets [][]string
+var packetsperSecond int = 0
 
-func Capture(handle *pcap.Handle, app *tview.Application, packetsView *tview.TextView, filterChan chan string, statusView *tview.TextView) {
+func Capture(handle *pcap.Handle, app *tview.Application, packetsView *tview.List, filterChan chan string, statusView *tview.TextView) {
 	go func() {
 		for newFilter := range filterChan {
-			if len(newFilter) >= 13 && newFilter[:13] == "-set-filename" {
+			if len(newFilter) >= 14 && newFilter[:13] == "-set-filename" {
 				filename = newFilter[14:]
 				h, m, s := time.Now().Clock()
 				statusView.Write([]byte(fmt.Sprintf("%v:%v:%v [green] INFO [white] ↓\nNew filename set: %s\n", h, m, s, filename)))
@@ -49,7 +52,8 @@ func Capture(handle *pcap.Handle, app *tview.Application, packetsView *tview.Tex
 				app.QueueUpdateDraw(func() {
 					h, m, s := time.Now().Clock()
 					statusView.Write([]byte(fmt.Sprintf("%v:%v:%v [green] INFO [white] ↓\nFilter applied: %v\n", h, m, s, newFilter)))
-					packetsView.SetText("")
+					packetsView.Clear()
+					packets = packets[:0]
 					statusView.ScrollToEnd()
 				})
 			}
@@ -58,76 +62,71 @@ func Capture(handle *pcap.Handle, app *tview.Application, packetsView *tview.Tex
 	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
 
 	for packet := range packetSource.Packets() {
+		info = make([]string, 3)
 		if !paused {
 			if write {
 				writer.WritePacket(packet.Metadata().CaptureInfo, packet.Data())
 				h, m, s := time.Now().Clock()
 				writeLog(app, statusView, fmt.Sprintf("%v:%v:%v [green] INFO [white] ↓\nPacket saved to file\n", h, m, s))
 			}
-			lines := strings.Split(packetsView.GetText(true), "\n")
-			if len(lines) > maxlines {
-				packetsView.SetText("")
+			if packetsView.GetItemCount() > maxlines {
+				packetsView.Clear()
+				packets = packets[:0]
 				h, m, s := time.Now().Clock()
 				statusView.Write([]byte(fmt.Sprintf("%v:%v:%v [green] INFO [white] ↓\nCleaned packets list\n", h, m, s)))
 				statusView.ScrollToEnd()
 
 			}
-			_, _, width, _ := packetsView.GetRect()
-			_, _, width1, _ := filterForm.GetRect()
-			filterForm.GetFormItemByLabel("Filter").(*tview.InputField).SetFieldWidth(width1 / 2)
+			_, _, width, _ := filterForm.GetRect()
+			filterForm.GetFormItemByLabel("Filter").(*tview.InputField).SetFieldWidth(width / 2)
 			if ipLayer := packet.Layer(layers.LayerTypeIPv4); ipLayer != nil {
 				ip, _ := ipLayer.(*layers.IPv4)
 				switch ip.Protocol {
 				case layers.IPProtocolTCP:
-					tcp++
+					tcpCounter++
 					tcp, _ := packet.Layer(layers.LayerTypeTCP).(*layers.TCP)
-					info := fmt.Sprintf(
-						"\n[lime]%-6s [green]%s:%d -> %s:%d\n[purple]TCP Flags: SYN=%t ACK=%t FIN=%t RST=%t\nSeq: %d Ack: %d Window: %d\nCheckSum: %d Urgent: %d\n[gray]%s",
-						ip.Protocol, ip.SrcIP, tcp.SrcPort, ip.DstIP, tcp.DstPort,
-						tcp.SYN, tcp.ACK, tcp.FIN, tcp.RST,
-						tcp.Seq, tcp.Ack, tcp.Window,
-						tcp.Checksum, tcp.Urgent,
-						strings.Repeat("─", width-2),
-					)
+					info[0] = fmt.Sprintf("%v[lime]%-6s [green]%s:%d -> %s:%d", packet.Metadata().Timestamp.Format("15:04:05.000 "), ip.Protocol, ip.SrcIP, tcp.SrcPort, ip.DstIP, tcp.DstPort)
+					info[1] = formatDetailTCP(tcp)
 
 					if appLayer := packet.ApplicationLayer(); appLayer != nil && payloads {
 						payload := appLayer.Payload()
 						if len(payload) > 0 {
-							info += fmt.Sprintf("[blue]Payload:\n%s", hex.Dump(payload))
+							info[2] = fmt.Sprintf("[blue]Payload:\n%s", hex.Dump(payload))
 						}
 					}
-					writeLog(app, packetsView, info)
 
 				case layers.IPProtocolUDP:
-					udp++
+					udpCounter++
 					udp, _ := packet.Layer(layers.LayerTypeUDP).(*layers.UDP)
-					info := fmt.Sprintf(
-						"\n[yellow]%-6s [green]%s:%d -> %s:%d\n[purple]Length: %d\nChecksum: %d\n[gray]%s",
-						ip.Protocol, ip.SrcIP, udp.SrcPort, ip.DstIP, udp.DstPort,
+					info[0] = fmt.Sprintf("%v [yellow]%-6s [green]%s:%d -> %s:%d", packet.Metadata().Timestamp.Format("15:04:05.000 "), ip.Protocol, ip.SrcIP, udp.SrcPort, ip.DstIP, udp.DstPort)
+					info[1] = fmt.Sprintf(
+						"[blue]Length: %d\nChecksum: %d\n[gray]",
 						udp.Length, udp.Checksum,
-						strings.Repeat("─", width-2),
 					)
-					writeLog(app, packetsView, info)
 
 				case layers.IPProtocolICMPv4:
-					icmp++
+					icmpCounter++
 					icmp, _ := packet.Layer(layers.LayerTypeICMPv4).(*layers.ICMPv4)
-					info := fmt.Sprintf(
-						"\n[red]%-6s [green]%s -> %s\nICMPv4 Type: %d Code: %d\nID: %d Seq: %d\nChecksum: %d\n[gray]%s",
-						ip.Protocol, ip.SrcIP, ip.DstIP,
+					info = make([]string, 3)
+					info[0] = fmt.Sprintf("%v[red]%-6s [green]%s -> %s", packet.Metadata().Timestamp.Format("15:04:05.000 "), ip.Protocol, ip.SrcIP, ip.DstIP)
+
+					info[1] = fmt.Sprintf(
+						"\nICMPv4 Type: %d Code: %d\nID: %d Seq: %d\nChecksum: %d\n[gray]",
 						icmp.TypeCode.Type(), icmp.TypeCode.Code(),
 						icmp.Id, icmp.Seq,
 						icmp.Checksum,
-						strings.Repeat("─", width-2),
 					)
-					writeLog(app, packetsView, info)
+					info[2] = ""
+
 				}
-				updateStatistic()
+				addPacketToList(app, packetsView, info)
 			}
+			updateStatistic(app)
+			packetsView.SetCurrentItem(packetsView.GetItemCount() - 1)
+			packetsperSecond++
 		}
 	}
 }
-
 func selectInterface(app *tview.Application) *pcap.Handle {
 	ifc, err := pcap.FindAllDevs()
 	if err != nil {
@@ -203,11 +202,63 @@ func savePacketsToFile() *pcapgo.Writer {
 	return pcapgoWriter
 }
 
-func updateStatistic() {
-	statistic.SetText(fmt.Sprintf(
-		"[lime]TCP: %d [yellow]UDP: %d [red]ICMP: %d",
-		tcp, udp, icmp,
-	))
+func updateStatistic(app *tview.Application) {
+	app.QueueUpdateDraw(func() {
+		statistic.SetText(fmt.Sprintf(
+			"total packages: %d\n %d/s\n[lime]TCP: %d [yellow]UDP: %d [red]ICMP: %d",
+			tcpCounter+udpCounter+icmpCounter, packetsperSecond, tcpCounter, udpCounter, icmpCounter,
+		))
+	})
+
+}
+
+func addPacketToList(app *tview.Application, packetsView *tview.List, info []string) {
+	packets = append(packets, info)
+	index := len(packets) - 1
+	_, _, width, _ := packetsView.GetInnerRect()
+	divider := "[gray]"
+	divider += strings.Repeat("─", width)
+	app.QueueUpdateDraw(func() {
+		if len(info) >= 2 {
+			packetsView.AddItem(fmt.Sprintf("[%d] %s", index, info[0]), divider, 0, nil)
+		} else if len(info) == 1 {
+			packetsView.AddItem(info[0], divider, 0, nil)
+		} else {
+			packetsView.AddItem("Unknown packet", "", 0, nil)
+		}
+		packetsView.SetCurrentItem(packetsView.GetItemCount() - 1)
+	})
+
+}
+
+func formatDetailTCP(tcp *layers.TCP) string {
+	flags := []string{}
+	if tcp.SYN {
+		flags = append(flags, "SYN")
+	}
+	if tcp.ACK {
+		flags = append(flags, "ACK")
+	}
+	if tcp.FIN {
+		flags = append(flags, "FIN")
+	}
+	if tcp.RST {
+		flags = append(flags, "RST")
+	}
+	details := fmt.Sprintf(
+		"[purple]%s[white]\n[purple]Seq=[white]%d  [purple]Ack=[white]%d\n[purple]Window=[white]%d  [purple]CheckSum=[white]%d  [purple]Urgent=[white]%d",
+		strings.Join(flags, " "),
+		tcp.Seq, tcp.Ack, tcp.Window, tcp.Checksum, tcp.Urgent,
+	)
+
+	return details
+}
+
+func formatDetail(detail []string) string {
+	if detail[2] == "" {
+		return fmt.Sprintf("%v\n%v\nthere is no payload", detail[0], detail[1])
+	}
+	return fmt.Sprintf("%v\n%v\n[blue]%v", detail[0], detail[1], detail[2])
 }
 
 func main() {
@@ -220,12 +271,8 @@ func main() {
 
 	filterChan := make(chan string, 10)
 	app := tview.NewApplication()
-	packetsView = tview.NewTextView().
-		SetDynamicColors(true).
-		SetScrollable(true).
-		SetChangedFunc(func() {
-			app.Draw()
-		})
+
+	packetsView = tview.NewList()
 
 	filterForm = tview.NewForm()
 	filterForm.SetButtonBackgroundColor(tcell.ColorGray)
@@ -276,22 +323,6 @@ func main() {
 
 [red::]fuck you.`
 	devView.SetText(aboutText)
-	var payloadButton *tview.Button
-
-	payloadButton = tview.NewButton("Show Payloads").
-		SetSelectedFunc(func() {
-			payloads = !payloads
-			h, m, s := time.Now().Clock()
-			statusView.Write([]byte(fmt.Sprintf("%v:%v:%v [green] INFO [white] ↓\nShow payloads: %v\n", h, m, s, payloads)))
-			if payloads {
-				payloadButton.SetLabel("Hide Payloads")
-			} else {
-				payloadButton.SetLabel("Show Payloads")
-			}
-			statusView.ScrollToEnd()
-		})
-
-	StyleButton(payloadButton)
 
 	var pauseButton *tview.Button
 
@@ -301,11 +332,9 @@ func main() {
 			if paused {
 				h, m, s := time.Now().Clock()
 				statusView.Write([]byte(fmt.Sprintf("%v:%v:%v [green] INFO [white] ↓\nOutput paused\n", h, m, s)))
-				packetsView.Write([]byte("Output paused\n"))
 				pauseButton.SetLabel("Resume")
 			} else {
 				pauseButton.SetLabel("Pause")
-				packetsView.Write([]byte("Output resumed\n"))
 				h, m, s := time.Now().Clock()
 				statusView.Write([]byte(fmt.Sprintf("%v:%v:%v [green] INFO [white] ↓\nOutput resumed\n", h, m, s)))
 			}
@@ -337,14 +366,22 @@ func main() {
 
 	commandsView := tview.NewFlex()
 	commandsView.SetDirection(tview.FlexRow)
-	commandsView.AddItem(payloadButton, 3, 1, true)
 	commandsView.AddItem(pauseButton, 3, 1, true)
 	commandsView.AddItem(saveButton, 3, 1, true)
 	commandsView.AddItem(quitButton, 3, 1, true)
-	commandsView.AddItem(statistic, 3, 0, false)
+	commandsView.AddItem(statistic, 10, 0, false)
 
 	commandsView.SetBorder(true)
 	commandsView.SetTitle("Commands")
+
+	detailView := tview.NewTextView()
+	detailView.SetDynamicColors(true)
+	detailView.SetBorder(true)
+	detailView.SetTitle("Details")
+
+	mainPanel := tview.NewFlex().SetDirection(tview.FlexColumn).
+		AddItem(packetsView, 0, 1, true).
+		AddItem(detailView, 0, 1, false)
 
 	rightPane := tview.NewFlex().SetDirection(tview.FlexRow).
 		AddItem(commandsView, 0, 1, false).
@@ -355,11 +392,11 @@ func main() {
 		AddItem(statusView, 0, 1, false)
 
 	leftPane := tview.NewFlex().SetDirection(tview.FlexRow).
-		AddItem(packetsView, 0, 3, false).
+		AddItem(mainPanel, 0, 3, false).
 		AddItem(bottomLeft, 0, 1, true)
 
 	layout := tview.NewFlex().
-		AddItem(leftPane, 0, 3, false).
+		AddItem(leftPane, 0, 4, false).
 		AddItem(rightPane, 0, 1, true)
 
 	app.SetRoot(layout, true)
@@ -367,7 +404,6 @@ func main() {
 	app.SetFocus(filterForm.GetFormItemByLabel("Filter"))
 	focusables := []tview.Primitive{
 		filterForm,
-		payloadButton,
 		pauseButton,
 		saveButton,
 		quitButton,
@@ -379,6 +415,23 @@ func main() {
 			currentFocus = (currentFocus + 1) % len(focusables)
 			app.SetFocus(focusables[currentFocus])
 			return nil
+		} else if event.Key() == tcell.Key(tcell.KeyCtrlC) {
+			app.Stop()
+			print("Ctrl+C pressed, exiting...\n")
+			os.Exit(0)
+		} else if event.Key() == tcell.Key(tcell.KeyCtrlP) {
+			app.SetFocus(packetsView)
+		} else if event.Key() == tcell.Key(tcell.KeyCtrlD) {
+			paused = !paused
+			if paused {
+				h, m, s := time.Now().Clock()
+				statusView.Write([]byte(fmt.Sprintf("%v:%v:%v [green] INFO [white] ↓\nOutput paused\n", h, m, s)))
+				pauseButton.SetLabel("Resume")
+			} else {
+				pauseButton.SetLabel("Pause")
+				h, m, s := time.Now().Clock()
+				statusView.Write([]byte(fmt.Sprintf("%v:%v:%v [green] INFO [white] ↓\nOutput resumed\n", h, m, s)))
+			}
 		}
 		return event
 
@@ -387,19 +440,20 @@ func main() {
 	defer handle.Close()
 	title := fmt.Sprintf("RawSniff > %s", ifcname)
 	packetsView.SetBorder(true).SetTitle(title).SetTitleColor(tcell.ColorGray)
-	packetsView.SetScrollable(true)
+	packetsView.SetSelectedStyle(
+		tcell.StyleDefault.
+			Background(tcell.NewHexColor(0x2e2e2e)).
+			Foreground(tcell.ColorDefault),
+	)
+	packetsView.SetChangedFunc(func(index int, mainText, secondaryText string, shortcut rune) {
+		detailView.SetText(formatDetail(packets[index]))
+	})
 	go Capture(handle, app, packetsView, filterChan, statusView)
-
 	go func() {
 		for {
-			if !paused {
-				app.QueueUpdateDraw(func() {
-					packetsView.Write([]byte("\n[white]Waiting for packets...\n"))
-				})
-				time.Sleep(5 * time.Second)
-			} else {
-				continue
-			}
+			time.Sleep(time.Second)
+			packetsperSecond = 0
+			updateStatistic(app)
 
 		}
 	}()
